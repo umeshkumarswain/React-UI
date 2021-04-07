e4f6d08ec363531e2aa8bded4242d3ad461f84fa
Umesh Swain <umesh.swain@siemens-healthineers.com>
2021-04-07 17:38:04 +0530

    added robo files

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='b tc'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
